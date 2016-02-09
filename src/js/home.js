fa0e44b5b973bcc9bf7269bf43741318345745b5
Dave Stevens <dave@slashwebstudios.com>
2016-02-08 20:13:21 -0500

    tried different require for velocity.

--- src/js/home.js
+++ src/js/home.js
@@ -1,11 +1,11 @@
-define(['jquery',
-        'velocity'],
-    function($,
-            velocity) {
+define(['jquery'],
+    function($) {
         return function() {
             self = this;
 
             self.init = function() {
+                require('velocity');
+                
                 console.log('inside init...');
                 $('.ship').velocity(
                     {translateY: -200},
