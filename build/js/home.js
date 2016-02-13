15cd1968cff81a633847b8f3e07a5bb1d7a0ad61
Dave Stevens <dave@slashwebstudios.com>
2016-02-13 14:34:38 -0500

    added screen shake.

--- build/js/home.js
+++ build/js/home.js
@@ -1,14 +1,34 @@
-define(['jquery', 'velocity'],
-    function($, velocity) {
+define(['jquery', 'velocity', 'velocityui'],
+    function($, velocity, velocityui) {
         return function() {
             self = this;
 
-            self.init = function() {
-                console.log('inside init...');
+            var _moveShip = function() {
                 $('.ship').velocity(
-                    {translateY: -200},
-                    {duration: 2000, easing: "swing"}
+                    {translateY: -600},
+                    {duration: 4000, easing: "easeInCubic"}
                 );
+            };
+
+            var _screenShake = function() {
+                var $mars = $('.mars');
+                var groundShake = [];
+                var SHAKE_X_MAGNITUDE = 5;
+                var SHAKE_Y_MAGNITUDE = 4;
+                var SHAKES = 199;
+                for (var i=SHAKES; i>0; i--) {
+                    var MAG_REDUCE = (i/SHAKES);
+                    var xDelta = Math.floor((Math.random() * SHAKE_X_MAGNITUDE - 2) * MAG_REDUCE);
+                    var yDelta = Math.floor(Math.random() * SHAKE_Y_MAGNITUDE * MAG_REDUCE);
+                    groundShake.push({e: $mars, p: {translateX: xDelta, translateY: yDelta}, o: {duration: 10}});
+                }
+                groundShake.push({e: $mars, p: {translateX: 0, translateY: 0}, o: {duration: 10}});
+                velocity.RunSequence(groundShake);
+            };
+
+            self.init = function() {
+                _moveShip();
+                _screenShake();
             };
         };
     }
