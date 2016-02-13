63267ffac0d2488bb01a0b003893b7c74b55e065
Dave Stevens <dave@slashwebstudios.com>
2016-02-13 16:43:08 -0500

    added thruster animation.

--- build/js/home.js
+++ build/js/home.js
@@ -12,7 +12,7 @@ define(['jquery', 'velocity', 'velocityui'],
 
             var _screenShake = function() {
                 var $mars = $('.mars');
-                var groundShake = [];
+                var sequence = [];
                 var SHAKE_X_MAGNITUDE = 5;
                 var SHAKE_Y_MAGNITUDE = 4;
                 var SHAKES = 199;
@@ -20,15 +20,29 @@ define(['jquery', 'velocity', 'velocityui'],
                     var MAG_REDUCE = (i/SHAKES);
                     var xDelta = Math.floor((Math.random() * SHAKE_X_MAGNITUDE - 2) * MAG_REDUCE);
                     var yDelta = Math.floor(Math.random() * SHAKE_Y_MAGNITUDE * MAG_REDUCE);
-                    groundShake.push({e: $mars, p: {translateX: xDelta, translateY: yDelta}, o: {duration: 10}});
+                    sequence.push({e: $mars, p: {translateX: xDelta, translateY: yDelta}, o: {duration: 10}});
                 }
-                groundShake.push({e: $mars, p: {translateX: 0, translateY: 0}, o: {duration: 10}});
-                velocity.RunSequence(groundShake);
+                sequence.push({e: $mars, p: {translateX: 0, translateY: 0}, o: {duration: 10}});
+                velocity.RunSequence(sequence);
+            };
+
+            var _fireThrusters = function() {
+                var $thruster = $('.thrust');
+                var sequence = [];
+                var SHAKE_Y_MAGNITUDE = 10;
+                var SHAKES = 199;
+                for (var i=SHAKES; i>0; i--) {
+                    var yDelta = Math.floor(Math.random() * SHAKE_Y_MAGNITUDE);
+                    sequence.push({e: $thruster, p: {translateY: yDelta}, o: {duration: 10}});
+                }
+                sequence.push({e: $thruster, p: {translateY: 0}, o: {duration: 10}});
+                velocity.RunSequence(sequence);
             };
 
             self.init = function() {
                 _moveShip();
                 _screenShake();
+                _fireThrusters();
             };
         };
     }
