e0480ca6c3d7978cbfe9fed7a35ab9f734b85242
Dave Stevens <dave@slashwebstudios.com>
2016-04-23 21:26:17 -0400

    added astronaut animation.

--- src/js/home.js
+++ src/js/home.js
@@ -1,17 +1,37 @@
-define(['jquery', 'velocity', 'velocityui', 'q'],
-    function($, velocity, velocityui, Q) {
+define(['jquery', 'velocity', 'velocityui', 'spriteanimator', 'q'],
+    function($, velocity, velocityui, spriteanimator, Q) {
         return function() {
             var self = this;
+            var _astronautAnimation = null;
+
+            var _resetScene = function(onComplete) {
+                _resetShip(function() {
+                    _closeWings();
+                    _resetAstronaut(function() {
+                        onComplete();
+                    });
+                });
+            };
+
+            var _resetAstronaut = function(onComplete) {
+                $('#astronaut').velocity(
+                    {translateY: 0, translateX: 0},
+                    {duration: 0, complete: onComplete }
+                );
+                $('#astronaut').show();
+            };
 
             var _resetShip = function(onComplete) {
                 $('.ship').velocity(
                     {translateY: 0},
                     {duration: 0, complete: onComplete }
                 );
+                $('.thrust').hide();
                 _resetWings();
             };
 
             var _resetWings = function() {
+                $(".replay-button").removeClass('show');
             };
 
             var _closeWings = function() {
@@ -42,6 +62,7 @@ define(['jquery', 'velocity', 'velocityui', 'q'],
 
             var _fireThrusters = function() {
                 var $thruster = $('.thrust');
+                $thruster.show();
                 var sequence = [];
                 var SHAKE_Y_MAGNITUDE = 10;
                 var SHAKES = 199;
@@ -54,21 +75,43 @@ define(['jquery', 'velocity', 'velocityui', 'q'],
             };
 
             var _playSpaceshipAnimation = function() {
-                $(".replay-button").removeClass('show');
-                _resetShip(function(elements) {
-                    _closeWings();
-                    _moveShip(function(elements) {
-                        $(".replay-button").addClass('show');
-                    });
+                _moveShip(function(elements) {
+                    $(".replay-button").addClass('show');
                 });
                 _screenShake();
                 _fireThrusters();
             };
 
+            var _playAstronautAnimation = function(onComplete) {
+                _astronautAnimation = $('#astronaut').spriteAnimator({
+                    cols: 10,
+                    rows: 3,
+                    cutOffFrames: 5
+                });
+                $('#astronaut').velocity(
+                    {translateX: 165, translateY: 25},
+                    {duration: 3000, easing: "linear", complete: onComplete }
+                );
+                _astronautAnimation.play({
+                    run: -1,
+                    delay: 20
+                });
+            };
+
+            var _playAnimation = function() {
+                _resetScene(function() {
+                    _playAstronautAnimation(function() {
+                        _astronautAnimation.stop();
+                        $('#astronaut').hide();
+                        _playSpaceshipAnimation();
+                    });
+                });
+            };
+
             self.init = function() {
-                _playSpaceshipAnimation();
+                _playAnimation();
                 $(".replay-button").click(function() {
-                    _playSpaceshipAnimation();
+                    _playAnimation();
                 });
             };
         };
