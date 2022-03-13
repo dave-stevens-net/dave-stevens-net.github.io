156e112e3a4feb2f55b27c848f9a4f469fdffec7
Dave Stevens <dave521@protonmail.com>
2022-03-13 09:10:50 -0400

    removed project images.

--- docs/App-GGSTM5R2.js
+++
@@ -1,1 +0,0 @@
-import{d as t,f as r,g as a}from"./chunk-BD32XUIH.js";var e=t(r()),o=t(a());function i(n,m,c){o.render(e.createElement(n,{assetsRoot:c}),document.getElementById(m))}export{i as init};
--- src/components/section-projects.tsx
+++ src/components/section-projects.tsx
@@ -1,14 +1,8 @@
 import * as React from 'react'
 import { styles } from './section-projects.styles'
-import { Button, Card, Classes, Dialog, Icon, Tag } from '@blueprintjs/core'
-import LazyImage from './lazy-image'
+import { AnchorButton, Card, Icon, Tag } from '@blueprintjs/core'
 
 const SectionProjects: React.FC = () => {
-  const [p1IsOpen, setP1IsOpen] = React.useState<boolean>(false)
-  const [p2IsOpen, setP2IsOpen] = React.useState<boolean>(false)
-  const [p3IsOpen, setP3IsOpen] = React.useState<boolean>(false)
-  const [p4IsOpen, setP4IsOpen] = React.useState<boolean>(false)
-
   return (
     <section className={styles.sectionLight}>
       <h2>Projects</h2>
@@ -16,7 +10,7 @@ const SectionProjects: React.FC = () => {
       <Card className={styles.projectCard}>
         <Icon icon="mobile-phone" className={styles.projectIcon} />
         <div>
-          <h5>Utility Company Billing / Usage iOS App</h5>
+          <h5>Talquin Electric iOS App</h5>
           <div className={styles.projectDatum}>
             <label>Duration:</label> Aug. 2017 - Dec. 2017
           </div>
@@ -31,12 +25,12 @@ const SectionProjects: React.FC = () => {
               programming language.
             </li>
           </ul>
-          <Button
+          <AnchorButton
             className={styles.viewButton}
             icon="share"
             outlined={true}
-            text="View Screenshots"
-            onClick={() => setP1IsOpen(true)}
+            text="View On App Store"
+            href="https://apps.apple.com/us/app/talquin/id1352526269"
           />
           <div className={styles.tagList}>
             <Tag icon="console" minimal={true} round={true}>
@@ -45,43 +39,11 @@ const SectionProjects: React.FC = () => {
           </div>
         </div>
       </Card>
-      <Dialog
-        isOpen={p1IsOpen}
-        title="Utility Billing / Usage iOS App"
-        className={`${styles.dialog} bp4-dark`}
-        canEscapeKeyClose={true}
-        canOutsideClickClose={true}
-        onClose={() => setP1IsOpen(false)}
-      >
-        <div className={`${styles.dialogBody} ${Classes.DIALOG_BODY}`}>
-          <LazyImage src="assets/img/p1_login.png" width="half" ht="100" />
-          <LazyImage
-            src="assets/img/p1_billing_history1.png"
-            width="half"
-            ht="100"
-          />
-          <LazyImage
-            src="assets/img/p1_billing_history2.png"
-            width="half"
-            ht="100"
-          />
-          <LazyImage
-            src="assets/img/p1_billing_history3.png"
-            width="half"
-            ht="100"
-          />
-          <Button
-            onClick={() => setP1IsOpen(false)}
-            text="Close"
-            outlined={true}
-          />
-        </div>
-      </Dialog>
 
       <Card className={styles.projectCard}>
         <Icon icon="mobile-phone" className={styles.projectIcon} />
         <div>
-          <h5>Utility Company Billing / Usage Android App</h5>
+          <h5>Talquin Electric Android App</h5>
           <div className={styles.projectDatum}>
             <label>Duration:</label> April 2018 - July 2018
           </div>
@@ -100,12 +62,12 @@ const SectionProjects: React.FC = () => {
               language.
             </li>
           </ul>
-          <Button
+          <AnchorButton
             className={styles.viewButton}
             icon="share"
             outlined={true}
-            text="View Screenshots"
-            onClick={() => setP2IsOpen(true)}
+            text="View On App Store"
+            href="https://play.google.com/store/apps/details?id=com.talquinelectric.android&hl=en_US"
           />
           <div className={styles.tagList}>
             <Tag icon="console" minimal={true} round={true}>
@@ -120,50 +82,11 @@ const SectionProjects: React.FC = () => {
           </div>
         </div>
       </Card>
-      <Dialog
-        isOpen={p2IsOpen}
-        title="Utility Billing / Usage Android App"
-        className={`${styles.dialog} bp4-dark`}
-        canEscapeKeyClose={true}
-        canOutsideClickClose={true}
-        onClose={() => setP2IsOpen(false)}
-      >
-        <div className={`${styles.dialogBody} ${Classes.DIALOG_BODY}`}>
-          <LazyImage src="assets/img/p2_login.png" width="half" ht="100" />
-          <LazyImage src="assets/img/p2_home.png" width="half" ht="100" />
-          <LazyImage
-            src="assets/img/p2_link_membership.png"
-            width="half"
-            ht="100"
-          />
-          <LazyImage
-            src="assets/img/p2_billing_history.png"
-            width="half"
-            ht="100"
-          />
-          <LazyImage
-            src="assets/img/p2_billing_history2.png"
-            width="half"
-            ht="100"
-          />
-          <LazyImage
-            src="assets/img/p2_billing_history3.png"
-            width="half"
-            ht="100"
-          />
-          <LazyImage src="assets/img/p2_add_card.png" width="half" ht="100" />
-          <Button
-            onClick={() => setP2IsOpen(false)}
-            text="Close"
-            outlined={true}
-          />
-        </div>
-      </Dialog>
 
       <Card className={styles.projectCard}>
         <Icon icon="desktop" className={styles.projectIcon} />
         <div>
-          <h5>Utility Company Billing / Usage Web App</h5>
+          <h5>Talquin Electric Web App</h5>
           <div className={styles.projectDatum}>
             <label>Duration:</label> Jan. 2018 - Nov. 2018
           </div>
@@ -178,13 +101,6 @@ const SectionProjects: React.FC = () => {
               card information for convenient billing payment.
             </li>
           </ul>
-          <Button
-            className={styles.viewButton}
-            icon="share"
-            outlined={true}
-            text="View Screenshots"
-            onClick={() => setP3IsOpen(true)}
-          />
           <div className={styles.tagList}>
             <Tag icon="console" minimal={true} round={true}>
               programming
@@ -198,35 +114,11 @@ const SectionProjects: React.FC = () => {
           </div>
         </div>
       </Card>
-      <Dialog
-        isOpen={p3IsOpen}
-        title="Utility Billing / Usage Web App"
-        className={`${styles.dialog} bp4-dark`}
-        canEscapeKeyClose={true}
-        canOutsideClickClose={true}
-        onClose={() => setP3IsOpen(false)}
-      >
-        <div className={`${styles.dialogBody} ${Classes.DIALOG_BODY}`}>
-          <LazyImage src="assets/img/p3_billing.png" width="wide" ht="300" />
-          <LazyImage src="assets/img/p3_charts_bar.png" width="wide" ht="300" />
-          <LazyImage src="assets/img/p3_charts_pie.png" width="wide" ht="300" />
-          <LazyImage
-            src="assets/img/p3_billing_payment_methods_empty.png"
-            width="wide"
-            ht="300"
-          />
-          <Button
-            onClick={() => setP3IsOpen(false)}
-            text="Close"
-            outlined={true}
-          />
-        </div>
-      </Dialog>
 
       <Card className={styles.projectCard}>
         <Icon icon="desktop" className={styles.projectIcon} />
         <div>
-          <h5>Utility Company Boil Water Notice Management Web App</h5>
+          <h5>Talquin Electric Boil Water Notice Management Web App</h5>
           <div className={styles.projectDatum}>
             <label>Duration:</label> May 2019 - Aug. 2019
           </div>
@@ -286,13 +178,6 @@ const SectionProjects: React.FC = () => {
               import processes using cloud functions.
             </li>
           </ul>
-          <Button
-            className={styles.viewButton}
-            icon="share"
-            outlined={true}
-            text="View Screenshots"
-            onClick={() => setP4IsOpen(true)}
-          />
           <div className={styles.tagList}>
             <Tag icon="console" minimal={true} round={true}>
               programming
@@ -306,50 +191,6 @@ const SectionProjects: React.FC = () => {
           </div>
         </div>
       </Card>
-      <Dialog
-        isOpen={p4IsOpen}
-        title="Insurance Marketer Web Portal"
-        className={`${styles.dialog} bp4-dark`}
-        canEscapeKeyClose={true}
-        canOutsideClickClose={true}
-        onClose={() => setP4IsOpen(false)}
-      >
-        <div className={`${styles.dialogBody} ${Classes.DIALOG_BODY}`}>
-          <LazyImage src="assets/img/p4_dashboard.png" width="wide" ht="300" />
-          <LazyImage
-            src="assets/img/p4_create_account_p1.png"
-            width="wide"
-            ht="300"
-          />
-          <LazyImage
-            src="assets/img/p4_create_account_p2.png"
-            width="wide"
-            ht="300"
-          />
-          <LazyImage src="assets/img/p4_city.png" width="wide" ht="300" />
-          <LazyImage src="assets/img/p4_post_bind.png" width="wide" ht="300" />
-          <LazyImage
-            src="assets/img/p4_tabs_permissions.png"
-            width="wide"
-            ht="300"
-          />
-          <LazyImage
-            src="assets/img/p4_user_detail.png"
-            width="wide"
-            ht="300"
-          />
-          <LazyImage
-            src="assets/img/p4_user_groups.png"
-            width="wide"
-            ht="300"
-          />
-          <Button
-            onClick={() => setP4IsOpen(false)}
-            text="Close"
-            outlined={true}
-          />
-        </div>
-      </Dialog>
     </section>
   )
 }
