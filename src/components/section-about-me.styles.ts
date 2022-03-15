85d46216a6c5cd31964fb23916734a8640fe76d3
Dave Stevens <dave521@protonmail.com>
2022-03-15 18:34:05 -0400

    Added more to the about me section.

--- src/components/section-about-me.styles.ts
+++ src/components/section-about-me.styles.ts
@@ -10,12 +10,31 @@ export const styles = {
     padding: 16px 16px 36px;
     width: 100%;
     background-color: ${Colors.BLACK};
+    h2 {
+      margin-bottom: 40px;
+    }
   `,
   row: css`
     display: flex;
+    width: 100%;
+    max-width: 600px;
+    @media (max-width: ${globalConstants.bp.phoneMax}) {
+      flex-direction: column;
+    }
   `,
   colAvatar: css`
     display: flex;
+    flex-direction: column;
+    align-items: center;
+    h3 {
+      font-family: ${globalConstants.titleFontFamily};
+      font-weight: ${globalConstants.titleFontWeight};
+      white-space: nowrap;
+      font-size: 24px;
+    }
+    @media (min-width: ${globalConstants.bp.tabletMin}) {
+      margin-right: 40px;
+    }
   `,
   avatar: css`
     width: 120px;
@@ -24,5 +43,11 @@ export const styles = {
   `,
   colHistory: css`
     display: flex;
+    flex-direction: column;
+    font-size: 14px;
+    line-height: 1.5;
+    p {
+      color: ${globalConstants.colorSubtext};
+    }
   `,
 }
--- src/components/section-about-me.tsx
+++ src/components/section-about-me.tsx
@@ -8,9 +8,30 @@ const SectionAboutMe: React.FC = () => {
       <div className={styles.row}>
         <div className={styles.colAvatar}>
           <img className={styles.avatar} src="assets/img/dave_stevens.jpg" />
+          <h3>Dave Stevens</h3>
         </div>
         <div className={styles.colHistory}>
-
+          <p>
+            Over the years I&rsquo;ve had the privilege of working with several
+            creative and successful teams. This experience has exposed me to
+            every layer of web application development, from database modeling
+            and service API design all the way up to UI graphic design and
+            development. While I would consider myself a full stack developer, I
+            specialize in front end development.
+          </p>
+          <h3>Educational Background</h3>
+          <p>
+            I have a Bachelor of Science degree in Computer Science from{' '}
+            <a
+              href="http://www.iastate.edu/"
+              itemScope={true}
+              itemProp="alumniOf"
+              itemType="http://schema.org/Organization"
+            >
+              <span itemProp="name">Iowa State University</span>
+            </a>
+            .
+          </p>
         </div>
       </div>
     </section>
